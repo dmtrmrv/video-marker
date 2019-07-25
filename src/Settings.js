2e64e541b11454369e0af9e3b25f89959a981ad2
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2019-07-25 10:18:05 -0400

    Clean-up

--- src/Settings.js
+++ src/Settings.js
@@ -11,9 +11,6 @@ const Settings = () => (
         <main role="main" className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
           <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
             <h1 className="h2">Settings</h1>
-            <div className="btn-toolbar mb-2 mb-md-0">
-              <button className="btn btn-sm btn-outline-secondary">Export</button>
-            </div>
           </div>
         </main>
       </div>
