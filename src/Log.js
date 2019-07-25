b9f97bf5a7a015f06be4666ebcdecf54cc6579ee
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2019-07-25 10:56:08 -0400

    Style the Log table

--- src/Log.js
+++ src/Log.js
@@ -16,128 +16,44 @@ const Log = () => (
             </div>
           </div>
           <div className="table-responsive">
-            <table className="table table-striped table-sm">
-              <thead>
+            <table className="table">
+              <thead className="thead-dark">
                 <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
+                  <th>Marker</th>
+                  <th>Start</th>
+                  <th>End</th>
+                  <th>Notes</th>
+                  <th>Action</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
+                  <td>Cam 1</td>
+                  <td>00:10:12</td>
+                  <td>00:12:53</td>
+                  <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+                  <td><button type="button" className="btn btn-outline-danger btn-sm">Delete</button></td>
                 </tr>
                 <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
-                  <td>elit</td>
+                  <td>Cam 1</td>
+                  <td>00:10:12</td>
+                  <td>00:12:53</td>
+                  <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+                  <td><button type="button" className="btn btn-outline-danger btn-sm">Delete</button></td>
                 </tr>
                 <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td>Praesent</td>
+                  <td>Cam 1</td>
+                  <td>00:10:12</td>
+                  <td>00:12:53</td>
+                  <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+                  <td><button type="button" className="btn btn-outline-danger btn-sm">Delete</button></td>
                 </tr>
                 <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td>ante</td>
-                </tr>
-                <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td>nisi</td>
-                </tr>
-                <tr>
-                  <td>1,005</td>
-                  <td>Nulla</td>
-                  <td>quis</td>
-                  <td>sem</td>
-                  <td>at</td>
-                </tr>
-                <tr>
-                  <td>1,006</td>
-                  <td>nibh</td>
-                  <td>elementum</td>
-                  <td>imperdiet</td>
-                  <td>Duis</td>
-                </tr>
-                <tr>
-                  <td>1,007</td>
-                  <td>sagittis</td>
-                  <td>ipsum</td>
-                  <td>Praesent</td>
-                  <td>mauris</td>
-                </tr>
-                <tr>
-                  <td>1,008</td>
-                  <td>Fusce</td>
-                  <td>nec</td>
-                  <td>tellus</td>
-                  <td>sed</td>
-                </tr>
-                <tr>
-                  <td>1,009</td>
-                  <td>augue</td>
-                  <td>semper</td>
-                  <td>porta</td>
-                  <td>Mauris</td>
-                </tr>
-                <tr>
-                  <td>1,010</td>
-                  <td>massa</td>
-                  <td>Vestibulum</td>
-                  <td>lacinia</td>
-                  <td>arcu</td>
-                </tr>
-                <tr>
-                  <td>1,011</td>
-                  <td>eget</td>
-                  <td>nulla</td>
-                  <td>Class</td>
-                  <td>aptent</td>
-                </tr>
-                <tr>
-                  <td>1,012</td>
-                  <td>taciti</td>
-                  <td>sociosqu</td>
-                  <td>ad</td>
-                  <td>litora</td>
-                </tr>
-                <tr>
-                  <td>1,013</td>
-                  <td>torquent</td>
-                  <td>per</td>
-                  <td>conubia</td>
-                  <td>nostra</td>
-                </tr>
-                <tr>
-                  <td>1,014</td>
-                  <td>per</td>
-                  <td>inceptos</td>
-                  <td>himenaeos</td>
-                  <td>Curabitur</td>
-                </tr>
-                <tr>
-                  <td>1,015</td>
-                  <td>sodales</td>
-                  <td>ligula</td>
-                  <td>in</td>
-                  <td>libero</td>
+                  <td>Cam 1</td>
+                  <td>00:10:12</td>
+                  <td>00:12:53</td>
+                  <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit</td>
+                  <td><button type="button" className="btn btn-outline-danger btn-sm">Delete</button></td>
                 </tr>
               </tbody>
             </table>
