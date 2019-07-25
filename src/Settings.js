28c099d6b81ce6381c6d2af108e9018c80a32809
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2019-07-25 10:38:49 -0400

    Add markup for settings page

--- src/Settings.js
+++ src/Settings.js
@@ -12,6 +12,27 @@ const Settings = () => (
           <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
             <h1 className="h2">Settings</h1>
           </div>
+          <form>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" placeholder="Marker Title" aria-label="Marker Title" />
+              <div className="input-group-append">
+                <button className="btn btn-outline-secondary" type="button">Remove</button>
+              </div>
+            </div>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" placeholder="Marker Title" aria-label="Marker Title" />
+              <div className="input-group-append">
+                <button className="btn btn-outline-secondary" type="button">Remove</button>
+              </div>
+            </div>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" placeholder="Marker Title" aria-label="Marker Title" />
+              <div className="input-group-append">
+                <button className="btn btn-outline-secondary" type="button">Remove</button>
+              </div>
+            </div>
+            <button type="button" className="btn btn-primary">Add Marker</button>
+          </form>
         </main>
       </div>
     </div>
