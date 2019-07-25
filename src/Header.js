8366728e0e8c4e28d1e19e212be4f8002b50bf25
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2019-07-25 11:06:47 -0400

    Rename to Video Marker

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => (
   <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <Link className="navbar-brand col-sm-3 col-md-2 mr-0" to="/">Video Notes</Link>
+    <Link className="navbar-brand col-sm-3 col-md-2 mr-0" to="/">Video Marker</Link>
     <ul className="navbar-nav px-3">
       <li className="nav-item text-nowrap">
         <a className="nav-link" href="#">v0.1.0</a>
