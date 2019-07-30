8dee84fd56274b95cfce5dfc53e11624782a4339
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2019-07-30 19:16:07 -0400

    Add production folder to the repo

---
+++ build/precache-manifest.3f37265a9477190cc2844c5211a1c6fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e41ff8b6eb860101ebb7b3ec6b94d33c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2a510eae648582d97d3",
+    "url": "/static/css/main.60c4ffbb.chunk.css"
+  },
+  {
+    "revision": "27d529fe5868b51e9f41",
+    "url": "/static/js/2.29c369f7.chunk.js"
+  },
+  {
+    "revision": "d2a510eae648582d97d3",
+    "url": "/static/js/main.a77e78be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
