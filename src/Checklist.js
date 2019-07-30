5e2c9740d9ea0c882775c2c0eb19bd0e7a4d1765
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2019-07-30 16:28:19 -0400

    Update Checklist

--- src/Checklist.js
+++ src/Checklist.js
@@ -12,6 +12,20 @@ const Checklist = () => (
           <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-3 mb-4 border-bottom">
             <h1 className="h2 mb-0">Checklist</h1>
           </div>
+          <div>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="defaultCheck1" />
+              <label className="form-check-label" for="defaultCheck1">
+                Tell everyone to relax.
+              </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="defaultCheck2" />
+              <label className="form-check-label" for="defaultCheck2">
+                Turn on <strong>Do Not Disturb</strong> mode on all devices.
+              </label>
+            </div>
+          </div>
         </main>
       </div>
     </div>
