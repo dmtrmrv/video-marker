bd73e3044a440840dfe02bdc028f53f253d3cd5d
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2019-08-01 11:42:35 -0500

    Minor tweaks to Settings page

--- src/Settings.js
+++ src/Settings.js
@@ -14,38 +14,42 @@ const Settings = (props) => {
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-3 mb-4 border-bottom">
         <h1 className="h2 mb-0">Settings</h1>
       </div>
-      <h2>Session Title</h2>
-      <input
-        type="text"
-        className="form-control"
-        aria-label={sessionTitle}
-        value={sessionTitle}
-        onChange={e => setSessionTitle(e.target.value)}
-      />
-      <h2>Markers</h2>
-      <form>
-        {buttons.map(button => (
-          <div className="input-group mb-3" key={button.id}>
-            <input
-              type="text"
-              className="form-control"
-              aria-label={button.title}
-              value={button.title}
-              onChange={e => editButton(button.id, e.target.value)}
-            />
-            <div className="input-group-append">
-              <button
-                className="btn btn-outline-danger"
-                type="button"
-                onClick={() => removeButton(button.id)}
-              >
-                Remove
-              </button>
+      <div className="mb-4">
+        <h2 className="h5">Session Title</h2>
+        <input
+          type="text"
+          className="form-control"
+          aria-label={sessionTitle}
+          value={sessionTitle}
+          onChange={e => setSessionTitle(e.target.value)}
+        />
+      </div>
+      <div className="mb-4">
+        <h2 className="h5">Markers</h2>
+        <form>
+          {buttons.map(button => (
+            <div className="input-group mb-3" key={button.id}>
+              <input
+                type="text"
+                className="form-control"
+                aria-label={button.title}
+                value={button.title}
+                onChange={e => editButton(button.id, e.target.value)}
+              />
+              <div className="input-group-append">
+                <button
+                  className="btn btn-outline-danger"
+                  type="button"
+                  onClick={() => removeButton(button.id)}
+                >
+                  Remove
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
-        <button type="button" className="btn btn-primary" onClick={addButton}>Add Marker</button>
-      </form>
+          ))}
+          <button type="button" className="btn btn-primary" onClick={addButton}>Add Marker</button>
+        </form>
+      </div>
     </>
   );
 };
